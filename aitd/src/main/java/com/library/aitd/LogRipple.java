65ed10e5c2fa5c83d8283b635a255767dd5ff312
451360508@qq.com <451360508@qq.com>
2021-01-09 11:10:06 +0800

    日志管理

--- aitd/src/main/java/com/library/aitd/LogRipple.java
+++ aitd/src/main/java/com/library/aitd/LogRipple.java
@@ -3,8 +3,15 @@ package com.library.aitd;
 import android.util.Log;
 
 public class LogRipple {
-    public static void e(String tag, Object content){
-        if (BuildConfig.DEBUG){
+
+    static boolean enable = BuildConfig.DEBUG;
+
+    public static void enableLog(boolean log) {
+        enable = log;
+    }
+
+    public static void e(String tag, Object content) {
+        if (enable) {
             Log.e(tag, content.toString());
         }
     }
