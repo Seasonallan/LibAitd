819705d3efe49ddffcd21fa494399cd3241733f9
451360508@qq.com <451360508@qq.com>
2021-03-05 14:45:06 +0800

    签名校验

--- app/src/test/java/com/library/aitd/Crypto_UnitTest.java
+++ app/src/test/java/com/library/aitd/Crypto_UnitTest.java
@@ -1,10 +1,17 @@
 package com.library.aitd;
 
+import com.ripple.core.binary.STReader;
 import com.ripple.core.coretypes.AccountID;
 import com.ripple.core.coretypes.Amount;
+import com.ripple.core.coretypes.Blob;
+import com.ripple.core.coretypes.Currency;
 import com.ripple.core.coretypes.uint.UInt32;
+import com.ripple.core.fields.Field;
+import com.ripple.core.serialized.BinaryParser;
+import com.ripple.core.types.known.tx.Transaction;
 import com.ripple.core.types.known.tx.signed.SignedTransaction;
 import com.ripple.core.types.known.tx.txns.Payment;
+import com.ripple.core.types.ledger.LedgerHeader;
 import com.ripple.crypto.ecdsa.Seed;
 
 import java.util.List;
@@ -34,11 +41,9 @@ public class Crypto_UnitTest extends BaseUnitTest {
         byte[] bytes = seed.keyPair().signMessage("你好".getBytes());
         boolean res = seed.keyPair().verifySignature("你好".getBytes(), bytes);
 
-        LogRipple.print("verifySignature: "+ res);
+        LogRipple.print("verifySignature: " + res);
 
-        if (true){
-            return;
-        }
+
         Payment payment = new Payment();
         payment.as(AccountID.Account, AitdOpenApi.Wallet.getAddress(seed.toString()));
         payment.as(AccountID.Destination, AitdOpenApi.Wallet.getAddress(seed2.toString()));
@@ -50,8 +55,22 @@ public class Crypto_UnitTest extends BaseUnitTest {
         SignedTransaction signed = payment.sign(seed.toString());
         String blob = signed.tx_blob;
 
+        STReader reader = new STReader(blob);
+        Payment paymentDecode = new Payment();
 
-       // onTestFinish(true);
+        //TODO:此处顺序需要调整
+        paymentDecode.put(Field.TransactionType.TransactionIndex, reader.uInt16());
+        paymentDecode.put(UInt32.Flags, reader.uInt32());
+        paymentDecode.put(UInt32.Sequence, reader.uInt32());
+        paymentDecode.put(UInt32.DestinationTag, reader.uInt32());
+        paymentDecode.put(UInt32.LedgerSequence, reader.uInt32());
+        paymentDecode.put(Amount.Amount, reader.amount());
+        paymentDecode.put(Amount.Fee, reader.amount());
+
+
+        LogRipple.print("verifySignature: " + paymentDecode.toJSON());
+
+        onTestFinish(true);
     }
 
 
